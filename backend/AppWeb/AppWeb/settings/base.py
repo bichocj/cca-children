acc0c8714f52dba765008071f75f15568050589c
Javier <bichocj@gmail.com>
2020-05-27 16:19:45 -0500

    update the v2

--- backend/AppWeb/AppWeb/settings/base.py
+++ backend/AppWeb/AppWeb/settings/base.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'common.apps.CommonConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.humanize',
-    'common.apps.CommonConfig',
     'accounts.apps.AccountsConfig',
     'dashboard.apps.DashboardConfig',
     'web.apps.WebConfig',
--- backend/AppWeb/common/admin.py
+++ backend/AppWeb/common/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+admin.site.site_header = 'App Name'
+admin.site.site_title = 'App Name'
+admin.site.index_title = 'App Name'
+
