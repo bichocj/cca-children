226fc06623146823a278b25e46402ed9f23841e6
Javier <bichocj@gmail.com>
2021-09-12 16:03:51 -0500

    added allowed hosts

--- backend/mysite/mysite/settings/heroku.py
+++ backend/mysite/mysite/settings/heroku.py
@@ -14,3 +14,9 @@ DATABASES['default'].update(db_from_env)
 
 WHITENOISE_STATIC_PREFIX = STATIC_URL
 STATIC_URL = os.environ.get('DJANGO_STATIC_HOST', STATIC_URL)
+
+ALLOWED_HOSTS = ['.herokuapp.com',]
+
+DEBUG = False
+
+SECURE_SSL_REDIRECT = True
