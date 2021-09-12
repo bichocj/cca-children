ee81c696412c1491a1e5e8b95bda08044ba95016
Javier <bichocj@gmail.com>
2021-09-12 15:32:42 -0500

    fix reqs

--- backend/mysite/mysite/settings/heroku.py
+++ backend/mysite/mysite/settings/heroku.py
@@ -1,1 +1,16 @@
+import os
+import dj_database_url
 from .base import * 
+
+INSTALLED_APPS += [
+    'storages',
+	'django.contrib.postgres',
+]
+
+# Database
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+WHITENOISE_STATIC_PREFIX = STATIC_URL
+STATIC_URL = os.environ.get('DJANGO_STATIC_HOST', STATIC_URL)
