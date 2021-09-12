c1dd3fcf8404ec264973c85fe2a96010d9f5237a
Javier <bichocj@gmail.com>
2021-09-12 17:47:13 -0500

    added whitenoise

--- backend/mysite/mysite/settings/heroku.py
+++ backend/mysite/mysite/settings/heroku.py
@@ -64,3 +64,4 @@ AWS_QUERYSTRING_AUTH = False
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
