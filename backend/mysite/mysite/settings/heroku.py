1b6b48494b33370e47070ed7566a106fba74f8c0
Javier <bichocj@gmail.com>
2021-09-12 20:19:49 -0500

    update pwd email

--- backend/mysite/mysite/settings/heroku.py
+++ backend/mysite/mysite/settings/heroku.py
@@ -67,7 +67,7 @@ STATICFILES_STORAGE = 'storages.backends.s3boto3.S3StaticStorage'
 #STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 EMAIL_HOST = 'smtp.sendgrid.net'
-EMAIL_HOST_USER = os.environ.get('SENDGRID_USERNAME')
-EMAIL_HOST_PASSWORD = os.environ.get('SENDGRID_PASSWORD')
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
