f0933139d3d75834d5536a11b8cf5782497af5d4
Javier <bichocj@gmail.com>
2022-04-15 18:12:16 -0500

    added filter

--- backend/mysite/dashboard/urls.py
+++ backend/mysite/dashboard/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, re_path
 from django.views.generic import TemplateView
-
+from django.contrib.auth.decorators import login_required
 from dashboard import views_people
 from dashboard import views_attendance
 from . import views
@@ -8,10 +8,10 @@ from . import views
 app_name = 'dashboard'
 
 urlpatterns = [
-  path('', TemplateView.as_view(template_name="dashboard/index.html")),
-  path('family/', TemplateView.as_view(template_name="dashboard/family.html"), name='family'),
+  path('', login_required(TemplateView.as_view(template_name="dashboard/index.html"))),
+  path('family/', login_required(TemplateView.as_view(template_name="dashboard/family.html")), name='family'),
   path('family/create/', views_people.family_create, name='family_create'),
-  path('attendance/', TemplateView.as_view(template_name="dashboard/attendance.html"), name='attendance'),  
+  path('attendance/', login_required(TemplateView.as_view(template_name="dashboard/attendance.html")), name='attendance'),  
   path('attendance/create/', views_attendance.create, name='attendance_create'),
   path('people/', views_people.show_all, name='people_show_all'),
   path('people/create/', views_people.create, name='people_create'),
